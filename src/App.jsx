bb2aa4ea8136f4091ab5ba6fd9b4bcbfe8b28679
muti-ur-rahman <tlearner25@gmail.com>
2024-09-30 02:41:42 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='flex flex-col '>
+      <div className=' bg-teal-500 text-4xl'>Password Generator</div>
+      <div className='bg-yellow-500 text-2xl text-center'>Craft Unique Passwords That Are Easy to Remember!</div>
+      </div>
+    </>
+  )
+}
+
+export default App
